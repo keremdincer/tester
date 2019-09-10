95d2d249e79da7c5277b12bb842e8a0a89889a18
Burak Kerem Dinçer <keremdincer@npi.local>
2019-09-10 18:10:11 +0300

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Index from './Pages/Index'
+import Test from './Pages/Test'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Index</Link>
+            </li>
+            <li>
+              <Link to='/test'>Test</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path='/' exact component={Index} />
+        <Route path='/test' component={Test} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
