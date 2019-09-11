6e25e576800fb2660ec6c924111cb21e3171e136
Burak Kerem Dinçer <keremdincer@npi.local>
2019-09-11 18:20:20 +0300

    Update Login page to use react-code-input.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,3 +1,55 @@
 import React from 'react'
 
-export default ({ match }) => <h2>Login with id: {match.params.id}</h2>
+import ReactCodeInput from 'react-code-input'
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  loginCard: {
+    width: '320px',
+    height: '180px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    textAlign: 'center',
+    background: 'white',
+    boxShadow: '0 0 5px rgba(0,0,0,.13)',
+    borderRadius: '6px'
+  },
+  description: {
+    marginBottom: '16px'
+  },
+  submitBtn: {
+    marginTop: '16px',
+    padding: '8px',
+    fontSize: '18px',
+    borderRadius: '6px',
+    border: '1px solid #ccc'
+  }
+}
+
+// ({ match }) =>
+
+export default class Login extends React.Component {
+
+  render () {
+    return (
+      <div style={styles.container}>
+        <div className="LoginCard" style={styles.loginCard}>
+          <div style={{ margin: '16px' }}>
+            <p style={styles.description}>Lütfen bu sayfaya ulaştığınız linkle birlikte gönderilen güvenlik kodunu giriniz.</p>
+            <form>
+              <ReactCodeInput autoFocus name='pin' type='tel' fields={6} onChange={(e) => console.log(e)} />
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
